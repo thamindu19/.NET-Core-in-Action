6e0a18c780f0ec7693f57b06ff5c269dd9ca5809
thamindu19 <92365822+thamindu19@users.noreply.github.com>
2023-01-04 10:49:19 +0530

    first commit

---
+++ CsvParser/Program copy 2.cs
@@ -0,0 +1,20 @@
+﻿// using System;
+// using System.IO;
+// using System.Linq;
+
+// namespace CsvParser
+// {
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             var reader = new StreamReader(new FileStream("Marvel.csv", FileModle.Open));
+//             var csvReader = new CsvReader(reader);
+//             foreach (var line in csvReader.Lines)
+//             {
+//                 Console.WriteLine(
+//                     line.First(p => p.Key == "Title").Value);
+//             }
+//         }
+//     }
+// }
